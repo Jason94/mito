41a255d86c2f2a3444a5c006f3fa87d3c6de26a5
Jason Walker <JasonW94@gmail.com>
2022-10-07 08:46:02 -0400

    Custom-validation test passing.

--- src/core/class/validations.lisp
+++ src/core/class/validations.lisp
@@ -19,11 +19,13 @@
 ;;;;     themselves are not responsible for implementing global validations
 ;;;;     parameters (such as :allow-nil), but are allowed to respond to them.
 ;;;; See mito.validations for examples.
+(declaim (optimize (speed 0) (space 0) (debug 3)))
 
 (in-package :cl-user)
 (defpackage mito.class.validations
   (:use #:cl)
-  (:export #:foobar))
+  (:export #:validated-class
+           #:validp))
 (in-package :mito.class.validations)
 
 (defclass validated-class (standard-class)
@@ -35,5 +37,38 @@
 (defmethod c2mop:validate-superclass ((class validated-class) (super standard-class))
   t)
 
-(defun foobar ()
-  10)
+(defun symbol-to-validate-func (symbol class)
+  "Get the function for a symbol with 'validate-' prepended."
+  (symbol-function (find-symbol (concatenate 'string "VALIDATE-" (string symbol))
+                                (symbol-package (class-name class)))))
+
+(defun validp (validated-obj)
+  "If valid returns:
+         t
+
+   If invalid returns (multiple values):
+         (nil ((:slot slot  :invalid-value invalid-value  :message message)
+               (:slot slot2 :invalid-value invalid-value2 :message message2)
+                ...))"
+  (let* ((validation-specs (slot-value (class-of validated-obj) 'validations))
+         (failure-specs (mapcar
+                          #'(lambda (validation-spec)
+                              (destructuring-bind (function-symbol slot-symbol &rest validation-args)
+                                                  validation-spec
+                                (multiple-value-bind (result fail-message)
+                                    (apply (symbol-to-validate-func function-symbol
+                                                                    (class-of validated-obj))
+                                           (nconc (list
+                                                    validated-obj
+                                                    slot-symbol
+                                                    (slot-value validated-obj slot-symbol))
+                                                  validation-args))
+                                  (if result
+                                    nil
+                                    (list :slot slot-symbol
+                                          :invalid-value (slot-value validated-obj slot-symbol)
+                                          :message fail-message)))))
+                          validation-specs)))
+    (if (every #'not failure-specs)
+      t
+      (values nil (remove-if #'not failure-specs)))))
--- t/class-validations.lisp
+++ t/class-validations.lisp
@@ -7,7 +7,37 @@
 
 (plan nil)
 
-(subtest "Ya boi"
-  (is (foobar) 10))
+(defun validate-positive (obj slot value &key &allow-other-keys)
+  (declare (ignore obj slot))
+  (if (>= value 0)
+    t
+    (values nil "must be positive")))
+
+(defun validate-not-empty (obj slot value &key &allow-other-keys)
+  (declare (ignore obj slot))
+  (if (string= "" value)
+    (values nil "must not be empty")
+    t))
+
+(defclass account ()
+  ((id
+     :initarg id
+     :accessor id
+    (name
+     :initarg :name
+     :accessor name))
+   (balance
+    :initarg :balance
+    :accessor balance
+    :initform 0))
+  (:metaclass validated-class)
+  (:validates
+    (positive balance)
+    (not-empty name)))
+
+(subtest "Passes validations."
+  (ok (validp (make-instance 'account
+                             :name "Steve"
+                             :balance 10))))
 
 (finalize)
